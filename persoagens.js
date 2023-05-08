196cf38484b1ce6f7ea7874f696d7889ab63c870
nobregathsa <thaisa.costa@sptech.school>
2023-05-08 13:37:59 -0300

    js

---
+++ persoagens.js
@@ -0,0 +1,103 @@
+function mostrar_conteudo_augustus(){
+    aparecer_personagens.innerHTML = `
+<div class="modal_willy">
+    <div class="fundo_modal_willy">
+        <div class="conteudo_modal_willy"> 
+            <img src="./vetores/willy_wonka_foto.jpg" width="300px" style="border-radius: 20px;">
+
+            <div class="texto">
+                <div class="fechar">
+                    <button onclick="fechar_modal()" class="botao_fechar">X</button>
+                </div>
+                <h1 style="font-weight: 900;">Willy Wonka</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis luctus orci, vitae elementum elit. Ut faucibus in neque a condimentum. Sed semper sapien eros, sit amet interdum dui pharetra vel. Proin malesuada eleifend rutrum. Aenean sodales, nunc sit amet venenatis vulputate, massa metus ullamcorper metus, eu hendrerit justo lorem ut libero. Proin sodales neque at sollicitudin sodales. Ut ullamcorper risus quis odio pharetra posuere. Vestibulum in velit pulvinar, maximus neque rutrum, mollis orci. Etiam vel elementum tortor, id interdum lacus. Ut tincidunt quis urna quis euismod. </p>
+            </div>
+        </div>
+    </div>
+</div>
+`
+}
+
+function mostrar_conteudo_violet(){
+    aparecer_personagens.innerHTML = `
+<div class="modal_willy">
+    <div class="fundo_modal_willy">
+        <div class="conteudo_modal_willy"> 
+            <img src="./vetores/willy_wonka_foto.jpg" width="300px" style="border-radius: 20px;">
+
+            <div class="texto">
+                <div class="fechar">
+                    <button onclick="fechar_modal()" class="botao_fechar">X</button>
+                </div>
+                <h1 style="font-weight: 900;">Willy Wonka</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis luctus orci, vitae elementum elit. Ut faucibus in neque a condimentum. Sed semper sapien eros, sit amet interdum dui pharetra vel. Proin malesuada eleifend rutrum. Aenean sodales, nunc sit amet venenatis vulputate, massa metus ullamcorper metus, eu hendrerit justo lorem ut libero. Proin sodales neque at sollicitudin sodales. Ut ullamcorper risus quis odio pharetra posuere. Vestibulum in velit pulvinar, maximus neque rutrum, mollis orci. Etiam vel elementum tortor, id interdum lacus. Ut tincidunt quis urna quis euismod. </p>
+            </div>
+        </div>
+    </div>
+</div>
+`
+}
+
+function mostrar_conteudo_veruca(){
+    aparecer_personagens.innerHTML = `
+<div class="modal_willy">
+    <div class="fundo_modal_willy">
+        <div class="conteudo_modal_willy"> 
+            <img src="./vetores/willy_wonka_foto.jpg" width="300px" style="border-radius: 20px;">
+
+            <div class="texto">
+                <div class="fechar">
+                    <button onclick="fechar_modal()" class="botao_fechar">X</button>
+                </div>
+                <h1 style="font-weight: 900;">Willy Wonka</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis luctus orci, vitae elementum elit. Ut faucibus in neque a condimentum. Sed semper sapien eros, sit amet interdum dui pharetra vel. Proin malesuada eleifend rutrum. Aenean sodales, nunc sit amet venenatis vulputate, massa metus ullamcorper metus, eu hendrerit justo lorem ut libero. Proin sodales neque at sollicitudin sodales. Ut ullamcorper risus quis odio pharetra posuere. Vestibulum in velit pulvinar, maximus neque rutrum, mollis orci. Etiam vel elementum tortor, id interdum lacus. Ut tincidunt quis urna quis euismod. </p>
+            </div>
+        </div>
+    </div>
+</div>
+`
+}
+
+function mostrar_conteudo_mike(){
+    aparecer_personagens.innerHTML = `
+<div class="modal_willy">
+    <div class="fundo_modal_willy">
+        <div class="conteudo_modal_willy"> 
+            <img src="./vetores/willy_wonka_foto.jpg" width="300px" style="border-radius: 20px;">
+
+            <div class="texto">
+                <div class="fechar">
+                    <button onclick="fechar_modal()" class="botao_fechar">X</button>
+                </div>
+                <h1 style="font-weight: 900;">Willy Wonka</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis luctus orci, vitae elementum elit. Ut faucibus in neque a condimentum. Sed semper sapien eros, sit amet interdum dui pharetra vel. Proin malesuada eleifend rutrum. Aenean sodales, nunc sit amet venenatis vulputate, massa metus ullamcorper metus, eu hendrerit justo lorem ut libero. Proin sodales neque at sollicitudin sodales. Ut ullamcorper risus quis odio pharetra posuere. Vestibulum in velit pulvinar, maximus neque rutrum, mollis orci. Etiam vel elementum tortor, id interdum lacus. Ut tincidunt quis urna quis euismod. </p>
+            </div>
+        </div>
+    </div>
+</div>
+`
+}
+
+function mostrar_conteudo_charlie(){
+    aparecer_personagens.innerHTML = `
+<div class="modal_willy">
+    <div class="fundo_modal_willy">
+        <div class="conteudo_modal_willy"> 
+            <img src="./vetores/willy_wonka_foto.jpg" width="300px" style="border-radius: 20px;">
+
+            <div class="texto">
+                <div class="fechar">
+                    <button onclick="fechar_modal()" class="botao_fechar">X</button>
+                </div>
+                <h1 style="font-weight: 900;">Willy Wonka</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis luctus orci, vitae elementum elit. Ut faucibus in neque a condimentum. Sed semper sapien eros, sit amet interdum dui pharetra vel. Proin malesuada eleifend rutrum. Aenean sodales, nunc sit amet venenatis vulputate, massa metus ullamcorper metus, eu hendrerit justo lorem ut libero. Proin sodales neque at sollicitudin sodales. Ut ullamcorper risus quis odio pharetra posuere. Vestibulum in velit pulvinar, maximus neque rutrum, mollis orci. Etiam vel elementum tortor, id interdum lacus. Ut tincidunt quis urna quis euismod. </p>
+            </div>
+        </div>
+    </div>
+</div>
+`
+}
+
+function fechar_modal(){
+    aparecer_personagens.innerHTML = " ";
+}
