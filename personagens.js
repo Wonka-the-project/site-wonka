e88a3999b43bd5ae9c31f53e8e1c5a2472d03988
nobregathsa <thaisa.costa@sptech.school>
2023-05-08 15:16:10 -0300

    atualizações + java script da tela personagens

--- personagens.js
+++ personagens.js
@@ -3,13 +3,13 @@ function mostrar_conteudo_augustus(){
 <div class="modal_willy">
     <div class="fundo_modal_willy">
         <div class="conteudo_modal_willy"> 
-            <img src="./vetores/willy_wonka_foto.jpg" width="300px" style="border-radius: 20px;">
+            <img src="./vetores/foto augustus.png" width="270px" style="border-radius: 20px;">
 
             <div class="texto">
                 <div class="fechar">
                     <button onclick="fechar_modal()" class="botao_fechar">X</button>
                 </div>
-                <h1 style="font-weight: 900;">Willy Wonka</h1>
+                <h1 style="font-weight: 900;">Augustus Gloop</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis luctus orci, vitae elementum elit. Ut faucibus in neque a condimentum. Sed semper sapien eros, sit amet interdum dui pharetra vel. Proin malesuada eleifend rutrum. Aenean sodales, nunc sit amet venenatis vulputate, massa metus ullamcorper metus, eu hendrerit justo lorem ut libero. Proin sodales neque at sollicitudin sodales. Ut ullamcorper risus quis odio pharetra posuere. Vestibulum in velit pulvinar, maximus neque rutrum, mollis orci. Etiam vel elementum tortor, id interdum lacus. Ut tincidunt quis urna quis euismod. </p>
             </div>
         </div>
@@ -23,13 +23,13 @@ function mostrar_conteudo_violet(){
 <div class="modal_willy">
     <div class="fundo_modal_willy">
         <div class="conteudo_modal_willy"> 
-            <img src="./vetores/willy_wonka_foto.jpg" width="300px" style="border-radius: 20px;">
+            <img src="./vetores/foto violet.png" width="270px" style="border-radius: 20px;">
 
             <div class="texto">
                 <div class="fechar">
                     <button onclick="fechar_modal()" class="botao_fechar">X</button>
                 </div>
-                <h1 style="font-weight: 900;">Willy Wonka</h1>
+                <h1 style="font-weight: 900;">Violet Beauregarde</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis luctus orci, vitae elementum elit. Ut faucibus in neque a condimentum. Sed semper sapien eros, sit amet interdum dui pharetra vel. Proin malesuada eleifend rutrum. Aenean sodales, nunc sit amet venenatis vulputate, massa metus ullamcorper metus, eu hendrerit justo lorem ut libero. Proin sodales neque at sollicitudin sodales. Ut ullamcorper risus quis odio pharetra posuere. Vestibulum in velit pulvinar, maximus neque rutrum, mollis orci. Etiam vel elementum tortor, id interdum lacus. Ut tincidunt quis urna quis euismod. </p>
             </div>
         </div>
@@ -43,13 +43,13 @@ function mostrar_conteudo_veruca(){
 <div class="modal_willy">
     <div class="fundo_modal_willy">
         <div class="conteudo_modal_willy"> 
-            <img src="./vetores/willy_wonka_foto.jpg" width="300px" style="border-radius: 20px;">
+            <img src="./vetores/foto veruca.png" width="270px" style="border-radius: 20px;">
 
             <div class="texto">
                 <div class="fechar">
                     <button onclick="fechar_modal()" class="botao_fechar">X</button>
                 </div>
-                <h1 style="font-weight: 900;">Willy Wonka</h1>
+                <h1 style="font-weight: 900;">Varuca Salt</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis luctus orci, vitae elementum elit. Ut faucibus in neque a condimentum. Sed semper sapien eros, sit amet interdum dui pharetra vel. Proin malesuada eleifend rutrum. Aenean sodales, nunc sit amet venenatis vulputate, massa metus ullamcorper metus, eu hendrerit justo lorem ut libero. Proin sodales neque at sollicitudin sodales. Ut ullamcorper risus quis odio pharetra posuere. Vestibulum in velit pulvinar, maximus neque rutrum, mollis orci. Etiam vel elementum tortor, id interdum lacus. Ut tincidunt quis urna quis euismod. </p>
             </div>
         </div>
@@ -63,13 +63,13 @@ function mostrar_conteudo_mike(){
 <div class="modal_willy">
     <div class="fundo_modal_willy">
         <div class="conteudo_modal_willy"> 
-            <img src="./vetores/willy_wonka_foto.jpg" width="300px" style="border-radius: 20px;">
+            <img src="./vetores/foto mike.png" width="270px" style="border-radius: 20px;">
 
             <div class="texto">
                 <div class="fechar">
                     <button onclick="fechar_modal()" class="botao_fechar">X</button>
                 </div>
-                <h1 style="font-weight: 900;">Willy Wonka</h1>
+                <h1 style="font-weight: 900;">Mike Teevee</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis luctus orci, vitae elementum elit. Ut faucibus in neque a condimentum. Sed semper sapien eros, sit amet interdum dui pharetra vel. Proin malesuada eleifend rutrum. Aenean sodales, nunc sit amet venenatis vulputate, massa metus ullamcorper metus, eu hendrerit justo lorem ut libero. Proin sodales neque at sollicitudin sodales. Ut ullamcorper risus quis odio pharetra posuere. Vestibulum in velit pulvinar, maximus neque rutrum, mollis orci. Etiam vel elementum tortor, id interdum lacus. Ut tincidunt quis urna quis euismod. </p>
             </div>
         </div>
@@ -83,13 +83,13 @@ function mostrar_conteudo_charlie(){
 <div class="modal_willy">
     <div class="fundo_modal_willy">
         <div class="conteudo_modal_willy"> 
-            <img src="./vetores/willy_wonka_foto.jpg" width="300px" style="border-radius: 20px;">
+            <img src="./vetores/foto charlie.png" width="270px" style="border-radius: 20px;">
 
             <div class="texto">
                 <div class="fechar">
                     <button onclick="fechar_modal()" class="botao_fechar">X</button>
                 </div>
-                <h1 style="font-weight: 900;">Willy Wonka</h1>
+                <h1 style="font-weight: 900;">Charlie Bucket</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis luctus orci, vitae elementum elit. Ut faucibus in neque a condimentum. Sed semper sapien eros, sit amet interdum dui pharetra vel. Proin malesuada eleifend rutrum. Aenean sodales, nunc sit amet venenatis vulputate, massa metus ullamcorper metus, eu hendrerit justo lorem ut libero. Proin sodales neque at sollicitudin sodales. Ut ullamcorper risus quis odio pharetra posuere. Vestibulum in velit pulvinar, maximus neque rutrum, mollis orci. Etiam vel elementum tortor, id interdum lacus. Ut tincidunt quis urna quis euismod. </p>
             </div>
         </div>
